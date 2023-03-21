446540090ff493cb9ab7271ddf4d4656f7d40371
mhaddaou <mhaddaou@student.1337.ma>
2023-03-21 19:58:01 +0000

    bubble sort

--- bubbleSort/main.cpp
+++ bubbleSort/main.cpp
@@ -5,44 +5,39 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mhaddaou <mhaddaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/21 05:46:50 by mhaddaou          #+#    #+#             */
-/*   Updated: 2023/03/21 05:56:08 by mhaddaou         ###   ########.fr       */
+/*   Created: 2023/03/21 19:32:21 by mhaddaou          #+#    #+#             */
+/*   Updated: 2023/03/21 19:56:39 by mhaddaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <vector>
+#include <list>
 
 template <typename iter>
 void bubbleSort(iter begin, iter end){
     bool swapped = true;
-
-    while (swapped){
+    for (; begin != end; ++begin){
         swapped = false;
-        for (iter i = begin; i != end - 1; i++){
-            if (*i > *(i + 1)){
-                std::swap(*i, *(i + 1));
+        iter it1 = begin;
+        for (iter it2 = begin; it2 != end; ++it2){
+            if (*it1 > *it2){
+                std::swap(*it1, *it2);
                 swapped = true;
             }
         }
-        end--;
     }
 }
 
 
 int main (){
-    std::vector<int> vec;
-    vec.push_back(2);
-    vec.push_back(8);
-    vec.push_back(5);
-    vec.push_back(3);
-    vec.push_back(9);
-    vec.push_back(4);
-    vec.push_back(1);
-
-    bubbleSort(vec.begin(), vec.end());
-    for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); it++) {
+    std::list<int> lst;
+    lst.push_back(10);
+    lst.push_back(4);
+    lst.push_back(5);
+    lst.push_back(1);
+    lst.push_back(0);
+    bubbleSort(lst.begin(), lst.end());
+    for (std::list<int>::iterator it = lst.begin(); it != lst.end();it++){
         std::cout << *it << " ";
     }
-
 }
